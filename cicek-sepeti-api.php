f7a9a1e4748f48e5eade5a5ed0f8f450d5f8ac22
Mehmet TOKMAK <33135266+mhmttkmk0352@users.noreply.github.com>
2020-07-08 16:09:49 +0300

    Create cicek-sepeti-api.php

---
+++ cicek-sepeti-api.php
@@ -0,0 +1,42 @@
+<?php 
+
+    class cicek_sepeti {
+
+        public $api_url;
+        private $api_key;
+        
+        public function __construct(){
+            $this->api_url = "http://apis.ciceksepeti.com";
+            $this->api_key = "cxlrcoRRC57w7YApZmZGy2r3VKV7iZp484jsorks";
+        }
+        public function siparis_getir(){
+            $post_verisi = array(
+                "startDate"=>"2020-07-03T03:52:09.390Z",
+                "endDate"=>"2020-07-07T03:52:09.390Z",
+                "pageSize"=>10,
+                "page"=>0
+            );
+
+            $post_verisi = json_encode( $post_verisi );
+
+            $ch = curl_init();
+            curl_setopt( $ch, CURLOPT_URL, $this->api_url."/api/v1/Order/GetOrders");
+            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1 );
+            curl_setopt( $ch, CURLOPT_HTTPHEADER, [
+                "x-api-key: ".$this->api_key,
+                "Content-Type: application/json"       
+            ]);
+            curl_setopt( $ch, CURLOPT_POST, 1 );
+            curl_setopt( $ch, CURLOPT_POSTFIELDS, ($post_verisi) );
+        
+            return curl_exec( $ch );        
+            
+        }
+    }
+
+    $cs = new cicek_sepeti;
+    echo $cs->siparis_getir();
+
+
+?>
